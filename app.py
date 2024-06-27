9c5187ffc19e67650c5ba9676d64fdb21b282146
fajrin1996 <rinsyarifudin@gmail.com>
2024-06-28 00:23:22 +0900

    membuat data cleansing

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import os
 import pandas as pd
-import csv
+
+import re
+import string
+from loop_csv import remove_agk, remove_emoj, remove_html, remove_punct, remove_url, remove_stopwords
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/upload'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
@@ -10,17 +14,37 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-@app.route('/file-csv/<nama>')
-def tampil_csv(nama):
-    result = []
-    df = pd.read_csv('static/upload/'+nama)
-    return render_template('tampil_csv.html',table = [df.to_html()] , title=[''])
 
-@app.route('/upload', methods=['POST'])
+@app.route('/file-csv/<nama>', methods=['GET', 'POST'])
+def tampil_csv(nama):
+    count = 0
+    df = pd.read_csv('static/upload/'+nama)
+    if request.method == 'POST':
+        df = pd.DataFrame(df['textDisplay'])
+        df
+        df['cleansing'] = df['textDisplay'].apply(lambda x: remove_url(x))
+        df['cleansing'] = df['cleansing'].apply(lambda x: remove_html(x))
+        df['cleansing'] = df['cleansing'].apply(lambda x: remove_emoj(x))
+        df['cleansing'] = df['cleansing'].apply(lambda x: remove_agk(x))
+        df['cleansing'] = df['cleansing'].apply(lambda x: remove_punct(x))
+        df['tokenz_and_caseFold'] = df['cleansing'].apply(lambda x: x.lower().split())
+        df['remove_stopwords'] = df['tokenz_and_caseFold'].apply(lambda x: remove_stopwords(x))
+        count += 1
+        # filebaru = 
+        return redirect(url_for('hasil',fileku=df.to_csv('df_baru'+str(count)+'.csv', index=False)))
+    else:
+        return render_template('tampil_csv.html',table = df.iterrows())
+
+@app.route('/tampil-hasil/<fileku>')
+def hasil(fileku):
+    read = pd.read_csv(fileku)
+    return render_template('cleansing-data.html', red = read.iterrows())
+
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if 'file' not in request.files:
         return 'No file part'
@@ -34,6 +58,14 @@ def upload_file():
         print(filename)
         return redirect(url_for('tampil_csv',nama=filename))
 
+
+    
+    
+
+        
+        
+
+        df
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ loop_csv.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import re
+import string
+from nltk.corpus import stopwords
+import nltk
+
+nltk.download('stopwords')
+stop_words = stopwords.words('indonesian')
+
+# data = pd.read_csv('/home/rins/web-development/NLP/static/upload/youtube-comments.csv')
+
+# for di, dt in data.iterrows():
+#     print(dt['authorDisplayName'])
+def remove_url(tweet):
+        url = re.compile(r'https?://\S|www\. \S+')
+        return url.sub(r'', tweet)
+
+def remove_html(tweet):
+        html = re.compile(r'<.*?>')
+        return html.sub(r'', tweet)
+
+def remove_emoj(tweet):
+        emoji_pattern = re.compile("["
+                                u"\U0001F600-\U0001F64F" # emoticons
+                                u"\U0001F300-\U0001F5FF" # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF" # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF" # flags (iOS)
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                "]+", flags=re.UNICODE)
+        return emoji_pattern.sub(r'', tweet)
+
+def remove_agk(tweet):
+        tweet = re.sub('[0-9]+', '', tweet)
+        tweet = re.sub(r'\$\W*', '', tweet)
+        tweet = re.sub(r'^RT[\s]', '', tweet)
+        tweet = re.sub(r'#', '', tweet)
+        return tweet
+def remove_punct(tweets):
+        translator = str.maketrans('', '', string.punctuation)
+        return tweets.translate(translator)
+
+def remove_stopwords(text):
+  return [word for word in text if word not in stop_words]
